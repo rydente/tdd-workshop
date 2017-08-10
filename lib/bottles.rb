369911ec9162b69eee21f25af279fb14d9fe7765
Ryan Y <ryan.yakstis@traderinteractive.com>
2017-08-10 18:05:24 -0400

    Pass all the tests
    Write the 99 bottles of beer song so we can call upon its power whenever
    we need to.
    
    Signed-off-by: Ryan Y <ryayak1460@protingumas.com>

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,3 +1,40 @@
 class Bottles
+  def verse(n)
+    now = Quantity.new(n)
+    theNext = Quantity.new(n - 1)
+    phrase = n == 0 ?
+               zeroPhrase :
+               "Take #{n == 1 ? 'it' : 'one'} down and pass it around, #{theNext}"
+    <<-VERSE
+#{now.to_s.capitalize} of beer on the wall, #{now} of beer.
+#{phrase} of beer on the wall.
+VERSE
+  end
+
+  def zeroPhrase
+    "Go to the store and buy some more, #{Quantity.new(99)}"
+  end
+
+  def verses(x, y)
+    demVerses = (y..x).map do |i|
+      verse i
+    end
+    demVerses.reverse.join("\n")
+  end
+
+  def song
+    verses(99, 0)
+  end
+
+  class Quantity
+    def initialize(n)
+      @quantity = n == 0 ? 'no more' : n.to_s
+      @ending = n == 1 ? '' : 's'
+    end
+
+    def to_s
+      "#{@quantity} bottle#{@ending}"
+    end
+  end
 
 end
